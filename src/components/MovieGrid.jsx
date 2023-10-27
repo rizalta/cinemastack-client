12cb3ee7e1bebf2fb1120dc0f1260d1ffeba5767
rizalta <rizalta00@gmail.com>
2023-10-27 18:00:22 +0530

    added tabs in homepage

--- src/components/MovieGrid.jsx
+++ src/components/MovieGrid.jsx
@@ -2,7 +2,7 @@ import MovieCard from "./MovieCard";
 
 const MovieGrid = ({ movies }) => {
   return (
-    <div className="w-full grid grid-cols-5 gap-5 z-0 pt-[100px] px-4">
+    <div className="w-full grid grid-cols-5 gap-5 z-0 pt-4 px-4">
       {movies.map((movie) => (
         <MovieCard movie={movie} key={movie.id}/>
       ))};
---
+++ src/pages/MovieDetails.jsx
@@ -0,0 +1,6 @@
+const MovieDetails = ({ movie }) => {
+  return (
+    <div>{movie.title}</div>
+  )
+}
+export default MovieDetails;
